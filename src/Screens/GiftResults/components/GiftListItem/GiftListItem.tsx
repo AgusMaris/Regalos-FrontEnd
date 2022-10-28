481e47300fad1f26f1fd06d60ca87b5821ecae5e
CarMatias <matiasbcar_31@hotmail.com>
2022-10-28 15:57:13 -0300

    Agrego boton de compra mercado libre

--- src/Screens/GiftResults/components/GiftListItem/GiftListItem.tsx
+++ src/Screens/GiftResults/components/GiftListItem/GiftListItem.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import { View, Text, Image, Dimensions } from 'react-native'
+import { View, Text, Image, Dimensions, TouchableOpacity, Button, Linking } from 'react-native'
 import colors from '../../../../Assets/colors'
 import { GiftSchema } from '../../../../schemas/Gift'
 export interface GiftListItemInterface {
   gift: GiftSchema
 }
+import { useLinkTo } from '@react-navigation/native';
+
 
 const WIDTH = Dimensions.get('window').width
 const HEIGHT = Dimensions.get('window').height
 
 const GiftListItem: React.FC<GiftListItemInterface> = ({ gift }) => {
+
   return (
     <View style={{ alignItems: 'center', marginTop: 20 }}>
       <View
@@ -34,9 +37,13 @@ const GiftListItem: React.FC<GiftListItemInterface> = ({ gift }) => {
               }}
             />
           )}
-          <Text style={{ color: colors.darkGrey, fontWeight: 'bold', fontSize: 18, marginTop: 10 }}>
-            {gift.name}
-          </Text>
+          <View style={{flex:1}}> 
+              <Text style={{ color: colors.darkGrey, fontWeight: 'bold', fontSize: 18, marginTop: 10 }}>
+                {gift.name}
+              </Text>
+               <TouchableOpacity onPress={() =>Linking.openURL("https://listado.mercadolibre.com.ar/"+gift.name)} style={{marginTop:20,alignSelf:'stretch',marginBottom:20,justifyContent:'flex-end',alignItems:'flex-end'}}>
+                    <Image source={require('../GiftListItem/mercadolibre.png')} style={{width:50,height:40,right:10}}></Image>
+                </TouchableOpacity></View>
         </View>
         <View
           style={{
@@ -50,8 +57,10 @@ const GiftListItem: React.FC<GiftListItemInterface> = ({ gift }) => {
           }}
         />
       </View>
+
     </View>
   )
 }
 
 export default GiftListItem
+
