8173ddd1522d80c70ca69d003a351a3b06f8976e
vidal77 <63751162+vidal77@users.noreply.github.com>
2022-10-28 13:04:34 -0300

    Termino feedback screen

--- src/Screens/Feedback/index.tsx
+++ src/Screens/Feedback/index.tsx
@@ -1,45 +1,49 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "../../Components/Container";
 import Background2 from "../../Components/Backgrounds/Background2";
 import { feedbackStyle } from "./style";
 import Ionicons from "@expo/vector-icons/Ionicons";
-
+import axios from "axios";
 type Props = {
 	setAuth: () => void;
 };
 
-const FeedbackScreen = ({ navigation }: Props) => {
-	const handleLike = () => {
-		console.log("like");
-		navigation.navigate("Home");
-		//TODO
-	};
+const url = "http://localhost:3000/uploadfeedback";
 
-	const handleDislike = () => {
-		console.log("dislike");
+const FeedbackScreen = ({ navigation }: Props) => {
+	const [id_usuario, setUsuario] = useState("");
+	const [id_regalo, setRegalo] = useState("");
+	const [calificacion, setCalificacion] = useState("");
+
+	const handleLike = () => {
 		navigation.navigate("Home");
-		//TODO
+		console.log("s");
+	};
+	const handleDislike = () => {
+		navigation.navigate("Home");
+		console.log("n");
 	};
 
 	return (
 		<Container>
 			<Background2 />
 			<View style={{ marginTop: 250 }}>
-				<Text style={feedbackStyle.text}>Te gusto el regalo?</Text>
-				<TouchableOpacity
-					style={feedbackStyle.button}
-					onPress={() => handleLike()}
-				>
-					Si
-					<Ionicons name="thumbs-up-outline" size={32} color="#fff" />
+				<Text style={feedbackStyle.question}>Te gusto el regalo?</Text>
+				<TouchableOpacity style={feedbackStyle.buttonYes} onPress={handleLike}>
+					<Text style={feedbackStyle.text}>
+						Si{"   "}
+						<Ionicons name="thumbs-up-outline" size={32} color="#fff" />
+					</Text>
 				</TouchableOpacity>
 				<TouchableOpacity
-					style={feedbackStyle.button}
-					onPress={() => handleDislike()}
+					style={feedbackStyle.buttonNo}
+					onPress={handleDislike}
 				>
-					No
-					<Ionicons name="thumbs-down-outline" size={32} color="#fff" />
+					<Text style={feedbackStyle.text}>
+						No{"   "}
+						<Ionicons name="thumbs-down-outline" size={32} color="#fff" />
+					</Text>
 				</TouchableOpacity>
 			</View>
 		</Container>
--- src/Screens/Feedback/style.ts
+++ src/Screens/Feedback/style.ts
@@ -2,21 +2,39 @@ import { StyleSheet } from 'react-native'
 import colors from '../../Assets/colors'
 
 export const feedbackStyle = StyleSheet.create({
-  button: {
-    backgroundColor: colors.primary,
+  buttonYes: {
+    borderRadius: 1,
+    borderWidth: 2,
+    borderColor: colors.lightGrey,
+    backgroundColor: "#4BB543",
     borderTopEndRadius: 20,
     borderBottomStartRadius: 20,
-    paddingHorizontal: 50,
+    paddingHorizontal: 60,
     paddingVertical: 15,
-    marginBottom: 75,
-    color: colors.white,
-    fontSize: 20,
-    fontWeight: '700',
+    marginBottom: 50, 
+  },
+  buttonNo: {
+    borderRadius: 1,
+    borderWidth: 2,
+    borderColor: colors.lightGrey,
+    backgroundColor: "#FC100D",
+    borderTopEndRadius: 20,
+    borderBottomStartRadius: 20,
+    paddingHorizontal: 60,
+    paddingVertical: 15,
+    marginBottom: 50, 
+  },
+  question: {
+    color: colors.primary,
+    fontSize: 32,
+    fontWeight: '900',
+    marginBottom: 50
   },
   text: {
-    color: colors.primary,
-    fontSize: 30,
-    fontWeight: '900',
-    marginBottom: 50 
+    textAlign: 'center',
+    right: 30,
+    color: colors.white,
+    fontSize: 28,
+    fontWeight: 'bold',
   }
 })
