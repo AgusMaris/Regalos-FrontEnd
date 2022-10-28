3a4c2eab6de6729a19d31bdfb02f95dd075eda8e
nahuel marro <nahuelmarro@gmail.com>
2022-10-28 01:26:35 -0300

    Completo el front del favoritos

--- src/Screens/GiftResults/components/GiftListItem/GiftListItem.tsx
+++ src/Screens/GiftResults/components/GiftListItem/GiftListItem.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
-import { View, Text, Image, Dimensions } from 'react-native'
+import { StylesContext } from '@material-ui/styles'
+import React, { useState } from 'react'
+import { View, Text, Image, Dimensions, StyleSheet, TouchableOpacity } from 'react-native'
+import Ionicons from '@expo/vector-icons/Ionicons';
 import colors from '../../../../Assets/colors'
 import { GiftSchema } from '../../../../schemas/Gift'
+import { Container } from '@material-ui/core';
+import { Button } from 'react-native-paper';
+
 export interface GiftListItemInterface {
   gift: GiftSchema
 }
@@ -9,49 +14,73 @@ export interface GiftListItemInterface {
 const WIDTH = Dimensions.get('window').width
 const HEIGHT = Dimensions.get('window').height
 
-const GiftListItem: React.FC<GiftListItemInterface> = ({ gift }) => {
-  return (
-    <View style={{ alignItems: 'center', marginTop: 20 }}>
+
+
+ 
+
+const GiftListItem: React.FC<GiftListItemInterface> = ({ gift }) => 
+{
+  const [fav,setFav]=useState(false)
+  return(
+  <View style={{ alignItems: 'center', marginTop: 20 }}>
+
+    <View
+      style={{
+        elevation: 8,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        width: WIDTH * 0.8,
+        height: HEIGHT / 6,
+      }}
+    >
+
+      <View style={{
+        backgroundColor: colors.primary,
+        position: 'relative',
+        bottom: 0,
+        left: 290,
+        alignItems: "stretch",
+        width: '10%',
+        height: 40,
+        borderBottomEndRadius: 20,
+        borderBottomStartRadius: 20,
+      }}>
+        <TouchableOpacity onPress={()=> setFav(!fav)}>
+          <Ionicons
+            name="star"
+            size={32}
+          color={fav ? "#f7a902": "#fff"} />
+        </TouchableOpacity>
+
+      </View>
+      <View style={{ flexDirection: 'row', flex: 1 }}>
+        {gift.imgSource !== '' && (
+          <Image
+            source={{ uri: gift.imgSource }}
+            style={{
+              marginTop: -15,
+              height: '100%',
+              width: '35%',
+              resizeMode: 'contain',
+            }} />
+        )}
+        <Text style={{ color: colors.darkGrey, fontWeight: 'bold', fontSize: 20, marginTop: 10 }}>
+          {gift.name}
+        </Text>
+      </View>
       <View
         style={{
-          elevation: 8,
-          backgroundColor: '#fff',
-          borderRadius: 10,
-          width: WIDTH * 0.8,
-          height: HEIGHT / 6,
-        }}
-      >
-        <View style={{ flexDirection: 'row', flex: 1 }}>
-          {gift.imgSource !== '' && (
-            <Image
-              source={{ uri: gift.imgSource }}
-              style={{
-                marginTop: 10,
-
-                height: '75%',
-                width: '50%',
-                resizeMode: 'contain',
-              }}
-            />
-          )}
-          <Text style={{ color: colors.darkGrey, fontWeight: 'bold', fontSize: 18, marginTop: 10 }}>
-            {gift.name}
-          </Text>
-        </View>
-        <View
-          style={{
-            backgroundColor: colors.primary,
-            position: 'absolute',
-            bottom: 0,
-            width: '100%',
-            height: 10,
-            borderBottomEndRadius: 10,
-            borderBottomStartRadius: 10,
-          }}
-        />
-      </View>
+          backgroundColor: colors.primary,
+          position: 'absolute',
+          bottom: 0,
+          width: '100%',
+          height: 10,
+          borderBottomEndRadius: 10,
+          borderBottomStartRadius: 10,
+        }} />
     </View>
-  )
+  </View>
+)
 }
 
 export default GiftListItem
